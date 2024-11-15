786d8a93af569712a9d00ac3a2bb74d2850e6dd3
keithhubner <32544724+keithhubner@users.noreply.github.com>
2024-11-15 11:43:36 +0000

    added tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <script defer data-domain="passwordvaultgenerator.com" src="https://plausible.io/js/script.file-downloads.js"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
