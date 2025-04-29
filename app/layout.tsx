8807036115a4b14915c6bbddd23fdee198e79357
keithhubner <keith@hubner.co.uk>
2025-04-29 15:19:15 +0100

    name update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,14 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Password Vault Generator',
+  description: 'Generate random test passwords for your applications',
+  
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
